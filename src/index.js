048be15a2122755851d7369836d0b84c96168f63
samip-ac <i60996395@gmail.com>
2024-03-30 19:50:20 +0800

    - ..

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/airmusic_function_intro_static_web">
+    {/* <BrowserRouter basename="/airmusic_function_intro_static_web"> */}
       <App />
-    </BrowserRouter>
+    {/* </BrowserRouter> */}
   </React.StrictMode>
 );
 
