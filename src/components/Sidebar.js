f8c8da5a02aae7fe1f0da9eba2009a3cce173891
samip-ac <i60996395@gmail.com>
2024-03-30 17:23:37 +0800

    - routes changed

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -6,8 +6,8 @@ import '../style/Sidebar.css'; // You'll create this CSS file to style the sideb
 const Sidebar = () => {
   return (
     <div className="sidebar">
-      <NavLink to="/" activeClassName="active">Main Page</NavLink>
-      <NavLink to="/1_realtime_baby_learning_mode" activeClassName="active">Realtime Baby Learning Mode</NavLink>
+      <NavLink to="/images_intro" activeClassName="active">Main Page</NavLink>
+      <NavLink to="/realtime_baby_learning_mode" activeClassName="active">Realtime Baby Learning Mode</NavLink>
       <NavLink to="/airmusic_function_intro_static_web" activeClassName="active">AirMusic Function Intro</NavLink>
       {/* Add more navigation links as needed */}
     </div>
