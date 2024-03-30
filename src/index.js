d8efa76dc49781dec880b00bf3d0e670f256e75b
samip-ac <i60996395@gmail.com>
2024-03-30 19:03:27 +0800

    - .....

--- src/index.js
+++ src/index.js
@@ -3,15 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename="/airmusic_function_intro_static_web">
-      <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={ <App /> }>
+        </Route>
+      </Routes>
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
  
 // If you want to start measuring performance in your app, pass a function
