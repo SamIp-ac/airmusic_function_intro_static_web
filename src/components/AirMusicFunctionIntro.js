357af9eca89e98a95943e579f4211a5da6386383
samip-ac <i60996395@gmail.com>
2024-03-30 18:15:03 +0800

    - ..button

--- src/components/AirMusicFunctionIntro.js
+++ src/components/AirMusicFunctionIntro.js
@@ -1,21 +1,27 @@
 // File: components/AirMusicFunctionIntro.js
-
-import React from 'react';
+import React, { useState } from 'react';
 import '../style/GeneralStyle.css';
 import Sidebar from './Sidebar'; // Import the Sidebar component
 
 const AirMusicFunctionIntro = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
   return (
     <div className="main-container">
-        <Sidebar />
-          <div className="content-container">
-            <h1>AirMusic Function Introduction</h1>
-            <h6 className="titleStyle">Updated at 30 March 2024 at 11:13:30 PM</h6>
-            <h6 className="titleStyle">"Some Text"</h6>
-            <h1>AirMusic App frontend Intro</h1>
-            <h6 className="titleStyle">"Some Text"</h6>
-        {/* Content for the introduction page */}
-      </div>
+        <Sidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar}/>
+        <div className={`content-container ${isSidebarOpen ? 'shifted' : ''}`}>
+          <button className="menu-button" onClick={toggleSidebar}>☰</button>
+          <h1>AirMusic Function Introduction</h1>
+          <h6 className="titleStyle">Updated at 30 March 2024 at 11:13:30 PM</h6>
+          <h6 className="titleStyle">"Some Text"</h6>
+          <h1>AirMusic App frontend Intro</h1>
+          <h6 className="titleStyle">"Some Text"</h6>
+          {/* Content for the introduction page */}
+        </div>
     </div>
   );
 };
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,11 +1,12 @@
 // Sidebar.js
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import '../style/Sidebar.css'; // You'll create this CSS file to style the sidebar
+import '../style/Sidebar.css';
 
-const Sidebar = () => {
+const Sidebar = ({ isOpen, toggleSidebar }) => {
   return (
-    <div className="sidebar">
+    <div className={`sidebar ${isOpen ? 'open' : 'closed'}`}>
+      <button className="toggle-button" onClick={toggleSidebar}>☰</button>
       <NavLink to="/airmusic_function_intro_static_web" activeClassName="active">AirMusic Function Frontend Intro</NavLink>
       <NavLink to="/images_intro" activeClassName="active">Image Intro Demo</NavLink>
       <NavLink to="/realtime_baby_learning_mode" activeClassName="active">Realtime Baby Learning Mode</NavLink>
