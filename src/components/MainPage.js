cdbfb99cdff61b9364e54285cc8f1865aede34f3
samip-ac <i60996395@gmail.com>
2024-03-30 18:17:52 +0800

    - .button update

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,16 +1,22 @@
 // MainPage.js
-import React from 'react';
+import React, { useState } from 'react';
 import main_function_intro_img from '../images/main_function_intro.png';
 import drawing_record from '../images/drawing_record.png';
 import '../style/GeneralStyle.css';
 import Sidebar from './Sidebar'; // Import the Sidebar component
 
 function MainPage() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+  
   return (
     <div className="main-container">
-      <Sidebar />
-      <div className="content-container">
-    
+      <Sidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
+      <div className={`content-container ${isSidebarOpen ? 'shifted' : ''}`}>
+        <button className="menu-button" onClick={toggleSidebar}>☰</button>
         <h2 className="titleStyle">Web Image Showcase</h2>
         <div>
           <h3 className="titleStyle">Different Showcase</h3>
--- src/components/RealtimeBabyLearningMode.js
+++ src/components/RealtimeBabyLearningMode.js
@@ -1,22 +1,29 @@
-import React from 'react';
+// RealtimeBabyLearningMode.js
+import React, { useState } from 'react';
 import realtime_baby_learning_mode_video from '../mp4/realtime_baby_learning_mode.mp4';
-import '../style/GeneralStyle.css';
 import '../style/GeneralStyle.css';
 import Sidebar from './Sidebar'; // Import the Sidebar component
 
 function RealtimeBabyLearningMode() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+  
   return (
     <div className="main-container">
-        <Sidebar />
-        <div className="content-container">
-        <h2 className="titleStyle">Showcase</h2>
-        <h3 className="titleStyle">Video Demonstrations</h3>
-        {/* List of videos to display */}
-        <video width="960" height="480" controls className="videoSize">
-          <source src={realtime_baby_learning_mode_video} type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
-      </div>
+        <Sidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
+        <div className={`content-container ${isSidebarOpen ? 'shifted' : ''}`}>
+          <button className="menu-button" onClick={toggleSidebar}>☰</button>
+          <h2 className="titleStyle">Showcase</h2>
+          <h3 className="titleStyle">Video Demonstrations</h3>
+          {/* List of videos to display */}
+          <video width="960" height="480" controls className="videoSize">
+            <source src={realtime_baby_learning_mode_video} type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        </div>
     </div>
   );
 }
